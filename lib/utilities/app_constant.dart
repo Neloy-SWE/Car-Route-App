47f9ac54ad3dfc9be9fab6a91fb5ebd55f12ab4d
Neloy-SWE <taufiqneloy.swe@gmail.com>
2025-10-10 12:20:56 +0600

    app constant class added

---
+++ lib/utilities/app_constant.dart
@@ -0,0 +1,15 @@
+/* 
+Created by Neloy on 10 October, 2025.
+Email: taufiqneloy.swe@gmail.com
+*/
+
+class AppConstant {
+  static const String origin = "origin";
+  static const String destination = "destination";
+  static const String osmTileUrl =
+      "https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png";
+  static const List<String> subdomains = ["a", "b", "c"];
+  static const double defaultLat = 23.79843409360955;
+  static const double defaultLong = 90.43531819831415;
+  static const double initialZoom = 13;
+}
