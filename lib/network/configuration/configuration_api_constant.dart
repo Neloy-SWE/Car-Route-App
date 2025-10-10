3572ca25f85f8fc01ed1ad8c89ef88216a1b9607
Neloy-SWE <taufiqneloy.swe@gmail.com>
2025-10-10 12:10:37 +0600

    api constant values added

---
+++ lib/network/configuration/configuration_api_constant.dart
@@ -0,0 +1,16 @@
+/* 
+Created by Neloy on 10 October, 2025.
+Email: taufiqneloy.swe@gmail.com
+*/
+
+class ConfigurationApiConstant {
+  static const String baseUrl =
+      "http://router.project-osrm.org/route/v1/driving/";
+  static const String overview = "overview";
+  static const String full = "full";
+  static const String geometries = "geometries";
+  static const String polyline = "polyline";
+  static const String checkYourInternet = "Check your internet.";
+  static const String carCannotGoThereDirectly =
+      "Car cannot go there directly.";
+}
